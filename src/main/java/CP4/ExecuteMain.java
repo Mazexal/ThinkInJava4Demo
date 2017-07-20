91b5a37b34de0725a525e8cbd8382db806bf8ef5
Mazexal <1023520044@qq.com>
2017-07-20 17:24:53 +0800

    CP1

--- src/main/java/CP4/ExecuteMain.java
+++ src/main/java/CP4/ExecuteMain.java
@@ -2,6 +2,7 @@ package CP4;
 
 import sun.font.TrueTypeFont;
 
+import java.lang.reflect.Array;
 import java.util.Random;
 
 /**
@@ -12,7 +13,10 @@ public class ExecuteMain {
 
         //practise_104();
         //practise_104_2();
-        practise_104_4();
+        //practise_104_4();
+       // practise_115_1();
+        practise_115_2();
+
     }
 
     public static void practise_104() {
@@ -55,6 +59,8 @@ public class ExecuteMain {
 
 
 
+
+
     public static void practise_104_4() {
         for(long i=2;i<100;i++){
             if(primeCheck(i)){
@@ -64,19 +70,46 @@ public class ExecuteMain {
     }
 
     //Fibonacci
-    public static void practise_115(){
+    public static void practise_115_1(){
 
-        int input=10;
-        int temp=0;
-        if(input<2){
-            System.out.print("1 ");
+        int input=10;//length
+        int[] ints=new int[input];
+        ints[0]=1;
+        ints[1]=1;
+        for(int i=2;i<10;i++){
+            ints[i]=ints[i-1]+ints[i-2];
         }
-        if(input<3){
-            System.out.print("1 ");
+        for(int i:ints){
+            System.out.println(i);
         }
-       for(int i=2;i<input;i++){
 
-       }
+    }
+
+    //吸血鬼函数
+    public static void practise_115_2(){
+        String[] targetNum = null;
+        String[] gunNum=null;   //目标数字和枪数字（即对比数字）
+        int sum = 0;   //吸血鬼数字的总个数
+        int count=0;   //有效判断次数，那些乘积不是4位数的就排除了
+        for (int i = 10; i < 100; i++) {
+            for (int j = i+1; j < 100; j++) {   //没有哪个两位数满足ab*ab=abab（不信可以编程验证），所以这里j从i+1开始就可以了
+                int i_target = i * j;
+                if (i_target < 1000 || i_target > 9999)
+                    continue; // 积不是4位数则跳过
+                count++;
+                targetNum = String.valueOf(i_target).split("");  //将其转换为一个字符串数组
+                gunNum = (String.valueOf(i) + String.valueOf(j)).split("");
+                java.util.Arrays.sort(targetNum);   //升序排列，因为只有排列了再比较才能保证不遗漏abcd=ba*dc这样的情况
+                java.util.Arrays.sort(gunNum);
+                if (java.util.Arrays.equals(targetNum, gunNum)) {
+                    // 排序后比较,为真则找到一组
+                    sum++;
+                    System.out.println("第" + sum + "个: " +  i_target+"="+i + "*" + j);
+                }
+            }
+        }
+        System.out.println("共进行了"+count+"次判断，找到" + sum + "个吸血鬼数字。");
+
     }
 
 
