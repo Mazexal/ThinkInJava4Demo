ae93cb43e0f3fe341bffc40feb6eb0ceceb5c786
Mazexal <1023520044@qq.com>
2017-07-20 15:08:09 +0800

    CP1

--- src/main/java/CP4/ExecuteMain.java
+++ src/main/java/CP4/ExecuteMain.java
@@ -1,20 +1,36 @@
 package CP4;
 
+import java.util.Random;
+
 /**
  * Created by Mazexal on 2017/7/20.
  */
 public class ExecuteMain {
-    public static void main(String [] args)
-    {
+    public static void main(String[] args) {
 
-        practise_104();
+        //practise_104();
+        practise_104_2();
     }
 
-    public static void practise_104()
-    {
-        for(int i=0;i<=100;i++)
-        {
+    public static void practise_104() {
+        for (int i = 0; i <= 100; i++) {
             System.out.println(i);
+        }
+    }
+
+    public static void practise_104_2() {
+        int temp = 0;
+        Random random = new Random();
+        for (int i = 0; i < 25; i++) {
+            int re = random.nextInt(100);
+            if(re>temp){
+                System.out.println(re +" 大于 " +temp);
+            }else if(re==temp){
+                System.out.println(re +" 等于 " +temp);
+            }else{
+                System.out.println(re +" 小于 " +temp);
+            }
+            temp = re;
         }
     }
 
