3394b07dc4c9f95b92a75e6ddc5cda82326d0b86
Mazexal <1023520044@qq.com>
2017-07-20 15:36:04 +0800

    CP1

--- src/main/java/CP4/ExecuteMain.java
+++ src/main/java/CP4/ExecuteMain.java
@@ -1,5 +1,7 @@
 package CP4;
 
+import sun.font.TrueTypeFont;
+
 import java.util.Random;
 
 /**
@@ -9,7 +11,8 @@ public class ExecuteMain {
     public static void main(String[] args) {
 
         //practise_104();
-        practise_104_2();
+        //practise_104_2();
+        practise_104_4();
     }
 
     public static void practise_104() {
@@ -23,16 +26,59 @@ public class ExecuteMain {
         Random random = new Random();
         for (int i = 0; i < 25; i++) {
             int re = random.nextInt(100);
-            if(re>temp){
-                System.out.println(re +" 大于 " +temp);
-            }else if(re==temp){
-                System.out.println(re +" 等于 " +temp);
-            }else{
-                System.out.println(re +" 小于 " +temp);
+            if (re > temp) {
+                System.out.println(re + " 大于 " + temp);
+            } else if (re == temp) {
+                System.out.println(re + " 等于 " + temp);
+            } else {
+                System.out.println(re + " 小于 " + temp);
+            }
+            temp = re;
+        }
+    }
+
+    public static void practise_104_3() {
+        int temp = 0;
+        Random random = new Random();
+        while (true) {
+            int re = random.nextInt(100);
+            if (re > temp) {
+                System.out.println(re + " 大于 " + temp);
+            } else if (re == temp) {
+                System.out.println(re + " 等于 " + temp);
+            } else {
+                System.out.println(re + " 小于 " + temp);
             }
             temp = re;
         }
     }
 
 
+
+    public static void practise_104_4() {
+        for(long i=2;i<100;i++){
+            if(primeCheck(i)){
+                System.out.println(i);
+            }
+        }
+    }
+
+
+
+    //高效素数探测 遍历只到开根号,
+    public static boolean primeCheck(long input) {
+        double end = Math.sqrt(new Double(input));
+
+        if (input < 3) {
+            return true;
+        } else {
+            for (double i = 2; i <= end; i++) {
+                double res = input % i;
+                if (res == 0) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
 }
